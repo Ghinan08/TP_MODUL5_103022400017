504eebe08b64915fe72aa412338a957cfbaadabb
Ghinan08 <alwaysongaming10@gmail.com>
2026-03-30 01:45:49 +0700

    menambahkan class DataGeneric

---
+++ TP_MODUL5_103022400017/DataGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL5_103022400017
+{
+    internal class DataGeneric<T>
+    {
+        public T Data { get; private set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+
+        }
+        public void PrintData()
+        {
+
+            Console.WriteLine("Data yang tersimpan adalah: " + Data);
+        }
+
+    }
+}
--- TP_MODUL5_103022400017/Program.cs
+++ TP_MODUL5_103022400017/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022400017;
+
+DataGeneric<string> dataNIM = new DataGeneric<string>("103022400017");
+dataNIM.PrintData();
